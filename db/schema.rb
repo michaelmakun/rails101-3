4bea800dd007a0a8f4addb3a41c5fba704738d0c
makun <michaelmakun@aliyun.com>
2017-03-24 12:42:38 +0800

    user can join/quit group

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170324023606) do
+ActiveRecord::Schema.define(version: 20170324034554) do
+
+  create_table "group_relationships", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "groups", force: :cascade do |t|
     t.integer  "user_id"

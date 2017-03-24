32cd4d2715ea21f48d6eff3092e805b61d07d94b
makun <michaelmakun@aliyun.com>
2017-03-24 11:38:54 +0800

    implement post CRUD

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,62 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @group = Group.find(params[:group_id])
+    @posts = Post.all
+  end
+
+  def show
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @group = Group.find(params[:group_id])
+    @post = Post.new
+  end
+
+  def create
+    @group = Group.find(params[:group_id])
+    @post = Post.new(post_params)
+    @post.user = current_user
+    @post.group = @group
+    if @post.save
+      redirect_to group_path(@group)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.user = current_user
+    @post.group = @group
+    if @post.update(post_params)
+      redirect_to group_path(@group), notice: "文章更新成功"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.group = @group
+    @post.user = current_user
+    @post.destroy
+    redirect_to group_path(@group), alert: "文章已删除"
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170324004443) do
+ActiveRecord::Schema.define(version: 20170324023606) do
 
   create_table "groups", force: :cascade do |t|
     t.integer  "user_id"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 20170324004443) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "group_id"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
